faf1de6b72ee1b567c4379a2b440feca2f468c28
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-30 15:19:00 +0100

    Added the variables on class Movie

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+    // Definita la classe Movie
+    class Movie{
 
-
+        // Dichiarate le variabili d'istanza
+        public $title;
+        public $genre;
+        public $year;
+        public $lang;
+    }
 ?>

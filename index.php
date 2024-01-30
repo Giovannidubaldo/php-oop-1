7c6adef1c38ccd82152619f180d550ccff6dea81
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-30 15:49:18 +0100

    Printed Movie on screen

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 
         // Definito un costruttore
         function __construct($_title, $_genre, $_year, $_lang){
-            $this->$title = $_title;
-            $this->$genre = $_genre;
-            $this->$year = $_year;
-            $this->$lang = $_lang;
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->year = $_year;
+            $this->lang = $_lang;
         }
 
         // Definisco un metodo
         public function printedFilm(){
-            return $this->title.", Genere: ".$this->genre", Anno di uscita: ".$this->year.", Lingua originale: ".$this->lang;
+            return "<div>".$this->title.". Genere: ".$this->genre.". Anno di uscita: ".$this->year.". Lingua originale: ".$this->lang."</div>";
         }
     }
 
@@ -26,4 +26,9 @@
     $movie_1 = new Movie("Inception", "Sci-fi", 2010, "Inglese");
     $movie_2 = new Movie("La vita è bella", "Commedia", 1997, "Italiano");
     $movie_3 = new Movie("Quasi amici - Intouchables", "Commedia", 2011, "Francese");
+
+    echo $movie_1->printedFilm();
+    echo $movie_2->printedFilm();
+    echo $movie_3->printedFilm();
+    
 ?>
